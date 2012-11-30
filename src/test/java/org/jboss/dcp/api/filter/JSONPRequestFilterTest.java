9a00cb375aec35a89032a63a90b68983c143280f
Vlastimil Elias <vlastimil.elias@worldonline.cz>
2012-11-30 10:57:39 +0100

    doFilter unit test improved

--- src/test/java/org/jboss/dcp/api/filter/JSONPRequestFilterTest.java
+++ src/test/java/org/jboss/dcp/api/filter/JSONPRequestFilterTest.java
@@ -6,7 +6,6 @@
 package org.jboss.dcp.api.filter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -65,33 +64,64 @@ public class JSONPRequestFilterTest {
 			// OK
 		}
 
-		// case - callback method defined
-		ServletOutputStream bos = Mockito.mock(ServletOutputStream.class);
-		Mockito.reset(reqMock, resMock, chainMock);
-		Mockito.when(reqMock.getParameter(PARAM_CALLBACK)).thenReturn("ahoj");
-		Mockito.when(resMock.getOutputStream()).thenReturn(bos);
-		Mockito.doAnswer(new Answer<Object>() {
+		// case - callback method defined, response over writer
+		{
+			ServletOutputStream bos = Mockito.mock(ServletOutputStream.class);
+			Mockito.reset(reqMock, resMock, chainMock);
+			Mockito.when(reqMock.getParameter(PARAM_CALLBACK)).thenReturn("ahoj");
+			Mockito.when(resMock.getOutputStream()).thenReturn(bos);
+			Mockito.doAnswer(new Answer<Object>() {
 
-			@Override
-			public Object answer(InvocationOnMock invocation) throws Throwable {
-				Object[] args = invocation.getArguments();
-				HttpServletResponse resp = (HttpServletResponse) args[1];
-				PrintWriter pw = resp.getWriter();
-				pw.write("ahoj response");
-				pw.write(" continue");
-				return null;
-			}
-		}).when(chainMock).doFilter(Mockito.eq(reqMock), Mockito.any(ServletResponse.class));
+				@Override
+				public Object answer(InvocationOnMock invocation) throws Throwable {
+					Object[] args = invocation.getArguments();
+					HttpServletResponse resp = (HttpServletResponse) args[1];
+					resp.getWriter().write("ahoj response");
+					resp.getWriter().write(" continue");
+					return null;
+				}
+			}).when(chainMock).doFilter(Mockito.eq(reqMock), Mockito.any(ServletResponse.class));
 
-		tested.doFilter(reqMock, resMock, chainMock);
-		Mockito.verify(resMock).setContentType(JSONPRequestFilter.CONTENT_TYPE);
-		Mockito.verify(resMock).setCharacterEncoding("UTF-8");
-		Mockito.verify(resMock, Mockito.times(3)).getOutputStream();
-		Mockito.verify(bos).write("ahoj(".getBytes());
-		Mockito.verify(bos).write("ahoj response continue".getBytes());
-		Mockito.verify(bos).write(");".getBytes());
-		Mockito.verifyNoMoreInteractions(bos);
-		Mockito.verifyNoMoreInteractions(resMock);
+			tested.doFilter(reqMock, resMock, chainMock);
+			Mockito.verify(resMock).setContentType(JSONPRequestFilter.CONTENT_TYPE);
+			Mockito.verify(resMock).setCharacterEncoding("UTF-8");
+			Mockito.verify(resMock, Mockito.times(3)).getOutputStream();
+			Mockito.verify(bos).write("ahoj(".getBytes());
+			Mockito.verify(bos).write("ahoj response continue".getBytes());
+			Mockito.verify(bos).write(");".getBytes());
+			Mockito.verifyNoMoreInteractions(bos);
+			Mockito.verifyNoMoreInteractions(resMock);
+		}
+
+		// case - callback method defined, response over output stream
+		{
+			ServletOutputStream bos = Mockito.mock(ServletOutputStream.class);
+			Mockito.reset(reqMock, resMock, chainMock);
+			Mockito.when(reqMock.getParameter(PARAM_CALLBACK)).thenReturn("ahoj");
+			Mockito.when(resMock.getOutputStream()).thenReturn(bos);
+			Mockito.doAnswer(new Answer<Object>() {
+
+				@Override
+				public Object answer(InvocationOnMock invocation) throws Throwable {
+					Object[] args = invocation.getArguments();
+					HttpServletResponse resp = (HttpServletResponse) args[1];
+					resp.getOutputStream().write("ahoj response".getBytes());
+					resp.getOutputStream().write(" continue".getBytes());
+					return null;
+				}
+			}).when(chainMock).doFilter(Mockito.eq(reqMock), Mockito.any(ServletResponse.class));
+
+			tested.doFilter(reqMock, resMock, chainMock);
+			Mockito.verify(resMock).setContentType(JSONPRequestFilter.CONTENT_TYPE);
+			Mockito.verify(resMock).setCharacterEncoding("UTF-8");
+			Mockito.verify(resMock, Mockito.times(3)).getOutputStream();
+			Mockito.verify(bos).write("ahoj(".getBytes());
+			Mockito.verify(bos).write("ahoj response continue".getBytes());
+			Mockito.verify(bos).write(");".getBytes());
+			Mockito.verifyNoMoreInteractions(bos);
+			Mockito.verifyNoMoreInteractions(resMock);
+		}
+
 	}
 
 	@Test
