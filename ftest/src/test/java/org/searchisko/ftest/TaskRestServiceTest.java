4f9c1dfa20c952141d967ac7c5adf04ac863c086
Libor Krzyzanek <libor@krzyzanek.com>
2014-04-29 14:03:14 +0200

    Added TaskRestServiceTest #12

--- ftest/src/test/java/org/searchisko/ftest/TaskRestServiceTest.java
+++ ftest/src/test/java/org/searchisko/ftest/TaskRestServiceTest.java
@@ -5,173 +5,155 @@
  */
 package org.searchisko.ftest;
 
+import com.jayway.restassured.http.ContentType;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 /**
- * Unit test for {@link TaskRestService}
+ * Integration test for Tasks REST API {@link org.searchisko.api.rest.TaskRestService}
  *
- * @author Vlastimil Elias (velias at redhat dot com)
+ * @author Libor Krzyzanek
  */
+@RunWith(Arquillian.class)
 public class TaskRestServiceTest {
 
-//	@Test
-//	public void getTypes() {
-//		TaskRestService tested = getTested();
-//		TaskManager managerMock = mockTaskManager(tested);
-//
-//		// case - ok
-//		{
-//			List<String> value = new ArrayList<String>();
-//			Mockito.when(managerMock.listSupportedTaskTypes()).thenReturn(value);
-//			Assert.assertEquals(value, tested.getTypes());
-//			Mockito.verify(managerMock).listSupportedTaskTypes();
-//		}
-//
-//		// case - exception
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.listSupportedTaskTypes()).thenThrow(new RuntimeException("test exception"));
-//			TestUtils.assertResponseStatus(tested.getTypes(), Status.INTERNAL_SERVER_ERROR);
-//		}
-//
-//	}
-//
-//	@Test
-//	public void getTasks() {
-//		TaskRestService tested = getTested();
-//		TaskManager managerMock = mockTaskManager(tested);
-//
-//		// case - ok no params
-//		{
-//			List<TaskStatusInfo> value = new ArrayList<TaskStatusInfo>();
-//			Mockito.when(managerMock.listTasks(null, null, 0, 0)).thenReturn(value);
-//			Assert.assertEquals(value, tested.getTasks(null, null, null, null));
-//			Mockito.verify(managerMock).listTasks(null, null, 0, 0);
-//		}
-//
-//		// case - params passing
-//		{
-//			Mockito.reset(managerMock);
-//			List<TaskStatusInfo> value = new ArrayList<TaskStatusInfo>();
-//			String[] ts = new String[] { TaskStatus.FAILOVER.toString() };
-//			List<TaskStatus> tslist = new ArrayList<TaskStatus>();
-//			tslist.add(TaskStatus.FAILOVER);
-//			Mockito.when(managerMock.listTasks("aa", tslist, 10, 5)).thenReturn(value);
-//			Assert.assertEquals(value, tested.getTasks("aa", ts, 10, 5));
-//			Mockito.verify(managerMock).listTasks("aa", tslist, 10, 5);
-//		}
-//
-//		// case - exception
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.listTasks(null, null, 0, 0)).thenThrow(new RuntimeException("test exception"));
-//			TestUtils.assertResponseStatus(tested.getTasks(null, null, 0, 0), Status.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//
-//	@Test
-//	public void getTask() {
-//		TaskRestService tested = getTested();
-//		TaskManager managerMock = mockTaskManager(tested);
-//
-//		// case - found
-//		{
-//			TaskStatusInfo value = new TaskStatusInfo();
-//			Mockito.when(managerMock.getTaskStatusInfo("myid")).thenReturn(value);
-//			Assert.assertEquals(value, tested.getTask("myid"));
-//			Mockito.verify(managerMock).getTaskStatusInfo("myid");
-//		}
-//
-//		// case - not found
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.getTaskStatusInfo("myid")).thenReturn(null);
-//			TestUtils.assertResponseStatus(tested.getTask("myid"), Status.NOT_FOUND);
-//			Mockito.verify(managerMock).getTaskStatusInfo("myid");
-//		}
-//
-//		// case - exception
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.getTaskStatusInfo("myid")).thenThrow(new RuntimeException("test exception"));
-//			TestUtils.assertResponseStatus(tested.getTask("myid"), Status.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//
-//	@SuppressWarnings("unchecked")
-//	@Test
-//	public void createTask() throws UnsupportedTaskException, TaskConfigurationException {
-//		TaskRestService tested = getTested();
-//		TaskManager managerMock = mockTaskManager(tested);
-//
-//		// case - ok
-//		{
-//			String value = "idsd";
-//			Map<String, Object> cfg = new HashMap<String, Object>();
-//			Mockito.when(managerMock.createTask("mytype", cfg)).thenReturn(value);
-//			Map<String, Object> result = (Map<String, Object>) tested.createTask("mytype", cfg);
-//			Assert.assertNotNull(result);
-//			Assert.assertEquals(value, result.get("id"));
-//			Mockito.verify(managerMock).createTask("mytype", cfg);
-//		}
-//
-//		// case - UnsupportedTaskException
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.createTask("mytype", null)).thenThrow(new UnsupportedTaskException("test exception"));
-//			TestUtils.assertResponseStatus(tested.createTask("mytype", null), Status.BAD_REQUEST);
-//		}
-//
-//		// case - TaskConfigurationException
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.createTask("mytype", null)).thenThrow(new TaskConfigurationException("test exception"));
-//			TestUtils.assertResponseStatus(tested.createTask("mytype", null), Status.BAD_REQUEST);
-//		}
-//
-//		// case - system exception
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.createTask("mytype", null)).thenThrow(new RuntimeException("test exception"));
-//			TestUtils.assertResponseStatus(tested.createTask("mytype", null), Status.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//
-//	@Test
-//	public void cancelTask() {
-//		TaskRestService tested = getTested();
-//		TaskManager managerMock = mockTaskManager(tested);
-//
-//		// case - ok
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.cancelTask("myid")).thenReturn(true);
-//			TestUtils.assertResponseStatus(tested.cancelTask("myid"), Status.OK, "Task canceled");
-//		}
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.cancelTask("myid")).thenReturn(false);
-//			TestUtils.assertResponseStatus(tested.cancelTask("myid"), Status.OK, "Task not canceled");
-//		}
-//
-//		// case - system exception
-//		{
-//			Mockito.reset(managerMock);
-//			Mockito.when(managerMock.cancelTask("myid")).thenThrow(new RuntimeException("test exception"));
-//			TestUtils.assertResponseStatus(tested.cancelTask("myid"), Status.INTERNAL_SERVER_ERROR);
-//		}
-//
-//	}
-//
-//	private TaskRestService getTested() {
-//		TaskRestService ret = new TaskRestService();
-//		ret.log = Logger.getLogger("testlogger");
-//		return ret;
-//	}
-//
-//	private TaskManager mockTaskManager(TaskRestService tested) {
-//		tested.taskService = Mockito.mock(TaskService.class);
-//		TaskManager value = Mockito.mock(TaskManager.class);
-//		Mockito.when(tested.taskService.getTaskManager()).thenReturn(value);
-//		return value;
-//	}
+	public static final String TASKS_REST_API = DeploymentHelpers.DEFAULT_REST_VERSION + "tasks";
+
+	@Deployment(testable = false)
+	public static WebArchive createDeployment() throws IOException {
+		return DeploymentHelpers.createDeployment();
+	}
+
+	@ArquillianResource
+	protected URL context;
+
+	@Test
+	@InSequence(0)
+	public void assertNotAuthenticated() throws MalformedURLException {
+		int expStatus = 401;
+
+		// GET /tasks/type
+		given().contentType(ContentType.JSON)
+				.expect().statusCode(expStatus)
+				.log().ifStatusCodeMatches(is(not(expStatus)))
+				.when().get(new URL(context, TASKS_REST_API + "/type").toExternalForm());
+
+		// GET /tasks/task
+		given().contentType(ContentType.JSON)
+				.expect().statusCode(expStatus)
+				.log().ifStatusCodeMatches(is(not(expStatus)))
+				.when().get(new URL(context, TASKS_REST_API + "/task").toExternalForm());
+
+		// GET /tasks/taskId
+		given().contentType(ContentType.JSON)
+				.expect().statusCode(expStatus)
+				.log().ifStatusCodeMatches(is(not(expStatus)))
+				.when().get(new URL(context, TASKS_REST_API + "/task/id").toExternalForm());
+
+		// POST /tasks/taskType e.g. reindex_from_persistence
+		given().contentType(ContentType.JSON)
+				.body("{\"sys_content_type\" : \"jbossorg_blog\"}")
+				.expect().statusCode(expStatus)
+				.log().ifStatusCodeMatches(is(not(expStatus)))
+				.when().post(new URL(context, TASKS_REST_API + "/task/reindex_from_persistence").toExternalForm());
+
+		// DELETE /tasks/task/id e.g. reindex_from_persistence
+		given().contentType(ContentType.JSON)
+				.expect().statusCode(expStatus)
+				.log().ifStatusCodeMatches(is(not(expStatus)))
+				.when().delete(new URL(context, TASKS_REST_API + "/task/id").toExternalForm());
+	}
+
+	@Test
+	@InSequence(10)
+	public void assertGetTasksType() throws MalformedURLException {
+		// GET /tasks/type
+		given().contentType(ContentType.JSON)
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.expect().statusCode(200)
+				.log().ifError()
+				.body("", containsInAnyOrder(
+						"reindex_from_persistence",
+						"renormalize_by_content_type",
+						"renormalize_by_project_code",
+						"renormalize_by_contributor_code",
+						"renormalize_by_contributor_lookup_id",
+						"renormalize_by_project_lookup_id",
+						"update_contributor_profile",
+						"reindex_contributor",
+						"reindex_project"
+				))
+				.when().get(new URL(context, TASKS_REST_API + "/type").toExternalForm());
+	}
+
+	@Test
+	@InSequence(11)
+	public void assertGetTasksTask() throws MalformedURLException {
+		// GET /tasks/task
+		given().contentType(ContentType.JSON)
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.expect().statusCode(200)
+				.log().ifError()
+				.body("size()", equalTo(0))
+				.when().get(new URL(context, TASKS_REST_API + "/task").toExternalForm());
+	}
+
+	static String taskID;
+
+	@Test
+	@InSequence(20)
+	public void assertReindexContributor() throws MalformedURLException {
+		// POST /tasks/task/reindex_contributor
+		taskID = given().contentType(ContentType.JSON)
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.body("{}")
+				.expect().statusCode(200)
+				.log().ifError()
+				.contentType(ContentType.JSON)
+				.body("id", is(not(empty())))
+				.when().post(new URL(context, TASKS_REST_API + "/task/reindex_contributor").toExternalForm())
+				.andReturn().body().jsonPath().get("id");
+		System.out.println("id:" + taskID);
+	}
+
+	@Test
+	@InSequence(21)
+	public void assertGetCreatedReindexContributorTask() throws MalformedURLException {
+		given().contentType(ContentType.JSON)
+				.pathParam("id", taskID)
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.expect().statusCode(200)
+				.log().ifError()
+				.body("id", is(taskID))
+				.body("taskType", is("reindex_contributor"))
+				.body("runCount", isA(Integer.class))
+				.body("taskStatus", notNullValue())
+				.body("cancelRequested", is(false))
+				.when().get(new URL(context, TASKS_REST_API + "/task/{id}").toExternalForm());
+	}
+
+
+	@Test
+	@InSequence(30)
+	public void assertDeleteCreatedReindexContributorTask() throws MalformedURLException {
+		given().contentType(ContentType.JSON)
+				.pathParam("id", taskID)
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.expect().statusCode(200)
+				.log().ifError()
+				.when().delete(new URL(context, TASKS_REST_API + "/task/{id}").toExternalForm());
+	}
 
 }
