188bf97876e40ed7ab07a9b2a4e9ec7509168b6c
Libor Krzyzanek <libor@krzyzanek.com>
2014-05-02 09:43:30 +0200

    Added /indexer rest api ftests covering unauthenticated operations #12

--- ftest/src/test/java/org/searchisko/ftest/IndexerRestServiceTest.java
+++ ftest/src/test/java/org/searchisko/ftest/IndexerRestServiceTest.java
@@ -5,14 +5,22 @@
  */
 package org.searchisko.ftest;
 
+import com.jayway.restassured.http.ContentType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Integration test for /indexer REST API.
@@ -25,7 +33,7 @@ import java.net.URL;
 @RunWith(Arquillian.class)
 public class IndexerRestServiceTest {
 
-	public static final String INDEXER_REST_API = DeploymentHelpers.DEFAULT_REST_VERSION + "indexer";
+	public static final String INDEXER_REST_API = DeploymentHelpers.DEFAULT_REST_VERSION + "indexer/{type}/{operation}";
 
 	@Deployment(testable = false)
 	public static WebArchive createDeployment() throws IOException {
@@ -35,6 +43,31 @@ public class IndexerRestServiceTest {
 	@ArquillianResource
 	URL context;
 
+	public final String[] types = {"elasticsearch-river-remote", "elasticsearch-river-jira"};
 
+	@Test
+	@InSequence(0)
+	public void assertNotAuthenticated() throws MalformedURLException {
+		int expStatus = 401;
 
+		for (String type : types) {
+			// GET /indexer/{type}/_status
+			given().contentType(ContentType.JSON)
+					.pathParam("type", type)
+					.pathParam("operation", "_status")
+					.expect().statusCode(expStatus)
+					.log().ifStatusCodeMatches(is(not(expStatus)))
+					.when().get(new URL(context, INDEXER_REST_API).toExternalForm());
+
+			// POST /indexer/{type}/_force_reindex
+			given().contentType(ContentType.JSON)
+					.pathParam("type", type)
+					.pathParam("operation", "_force_reindex")
+					.expect().statusCode(expStatus)
+					.log().ifStatusCodeMatches(is(not(expStatus)))
+					.when().post(new URL(context, INDEXER_REST_API).toExternalForm());
+		}
+	}
+
+	//TODO: FTEST: IndexerRestServiceTest: Test start reindex
 }
