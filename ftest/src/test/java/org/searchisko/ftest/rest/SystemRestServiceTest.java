2cdf47ccc23d326bc8e24adb85ac2bf0442efd62
Libor Krzyzanek <libor@krzyzanek.com>
2014-05-27 15:25:06 +0200

    Added more tests on auditlog however they are ignored because of missing mapping

--- ftest/src/test/java/org/searchisko/ftest/rest/SystemRestServiceTest.java
+++ ftest/src/test/java/org/searchisko/ftest/rest/SystemRestServiceTest.java
@@ -10,15 +10,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Logger;
 
+import com.jayway.restassured.builder.ResponseSpecBuilder;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.specification.ResponseSpecification;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.searchisko.ftest.DeploymentHelpers;
+import org.searchisko.ftest.ProviderModel;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -59,13 +63,14 @@ public class SystemRestServiceTest {
 				.when().get(new URL(context, SYSTEM_REST_API).toExternalForm());
 	}
 
+	// TODO: Create index and push mapping
+
 	@Test
 	@InSequence(5)
 	public void assertAuditLogIndexIsMissing() throws MalformedURLException {
 		given().pathParam("operation", OPERATION_AUDITLOG).contentType(ContentType.JSON)
 				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
 				.expect().statusCode(204)
-//				.log().all()
 				.when().get(new URL(context, SYSTEM_REST_API).toExternalForm());
 	}
 
@@ -128,5 +133,45 @@ public class SystemRestServiceTest {
 				.when().get(new URL(context, SYSTEM_REST_API).toExternalForm());
 	}
 
+	public static final ProviderModel provider1 = new ProviderModel("provider1", "Password1");
+
+	@Test
+	@InSequence(120)
+	public void assertNewProvider1() throws MalformedURLException {
+		ProviderRestServiceTest.createNewProvider(context, provider1);
+	}
+
+	@Test
+	@InSequence(121)
+	public void assertRefreshES2() throws MalformedURLException {
+		DeploymentHelpers.refreshES();
+	}
+
+	static ResponseSpecification auditProvider1ResponseSpec = new ResponseSpecBuilder()
+			.expectStatusCode(200)
+			.expectContentType(ContentType.JSON)
+			.expectBody("x.y.size()", is(2))
+			.expectBody("total", is(1))
+			.expectBody("hits[0].data.id", is(provider1.name))
+			.expectBody("hits[0].data.content.name", is(provider1.name))
+			.expectBody("hits[0].data.operation", is("POST"))
+			.expectBody("hits[0].data.user_type", is("PROVIDER"))
+			.expectBody("hits[0].data.type", is("audit"))
+			.expectBody("hits[0].data.date", notNullValue())
+			.build();
+
+	@Test
+	@InSequence(122)
+	@Ignore("Filtering doesn't work because it needs ES mapping")
+	public void assertAuditFilterByOperation() throws MalformedURLException {
+		given().pathParam("operation", OPERATION_AUDITLOG).contentType(ContentType.JSON)
+				.queryParam("operation", "POST")
+				.auth().basic(DeploymentHelpers.DEFAULT_PROVIDER_NAME, DeploymentHelpers.DEFAULT_PROVIDER_PASSWORD)
+				.log().all()
+				.expect()
+				.log().all()
+				.spec(auditProvider1ResponseSpec)
+				.when().get(new URL(context, SYSTEM_REST_API).toExternalForm());
+	}
 
 }
