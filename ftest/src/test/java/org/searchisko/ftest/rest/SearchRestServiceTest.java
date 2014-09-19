873d0c332136db91e09bfa9866cc9cbabd44a3ac
Vlastimil Elias <vlastimil.elias@worldonline.cz>
2014-09-19 13:22:09 +0200

    added note about problems with JDK 1.7.0_67 during ftest run

--- ftest/src/test/java/org/searchisko/ftest/rest/SearchRestServiceTest.java
+++ ftest/src/test/java/org/searchisko/ftest/rest/SearchRestServiceTest.java
@@ -11,7 +11,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.jayway.restassured.http.ContentType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.junit.InSequence;
@@ -23,12 +22,14 @@ import org.junit.runner.RunWith;
 import org.searchisko.ftest.DeploymentHelpers;
 import org.searchisko.ftest.ProviderModel;
 
+import com.jayway.restassured.http.ContentType;
+
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 /**
  * Integration test for /search REST API.
- *
+ * 
  * @author Libor Krzyzanek
  * @see org.searchisko.api.rest.SearchRestService
  */
@@ -48,15 +49,9 @@ public class SearchRestServiceTest {
 	@Test
 	@InSequence(10)
 	public void assertSearchAllNoResult() throws MalformedURLException {
-		given().contentType(ContentType.JSON)
-				.expect()
-				.log().ifError()
-				.statusCode(200)
-				.contentType(ContentType.JSON)
-				.body("hits.total", is(0))
-				.when().get(new URL(context, SEARCH_REST_API).toExternalForm());
+		given().contentType(ContentType.JSON).expect().log().ifError().statusCode(200).contentType(ContentType.JSON)
+				.body("hits.total", is(0)).when().get(new URL(context, SEARCH_REST_API).toExternalForm());
 	}
-
 
 	public static final String TYPE1 = "provider1_blog";
 
@@ -90,7 +85,6 @@ public class SearchRestServiceTest {
 		ContentRestServiceTest.createOrUpdateContent(context, provider1, TYPE1, contentId2, content);
 	}
 
-
 	@Test
 	@InSequence(40)
 	public void assertRefreshES() throws MalformedURLException {
@@ -102,16 +96,12 @@ public class SearchRestServiceTest {
 	@Test
 	@InSequence(41)
 	public void assertSearchAllInsertedContent() throws MalformedURLException {
-		uuid = given().contentType(ContentType.JSON)
-				.expect()
-				.log().all()
-				.statusCode(200)
-				.contentType(ContentType.JSON)
-				.body("hits.total", is(2))
-				.body("hits.hits[0]._type", is(TYPE1))
-				.body("hits.hits[1]._type", is(TYPE1))
-				.when().get(new URL(context, SEARCH_REST_API).toExternalForm()).andReturn().getBody().jsonPath().getString("uuid");
+		uuid = given().contentType(ContentType.JSON).expect().log().all().statusCode(200).contentType(ContentType.JSON)
+				.body("hits.total", is(2)).body("hits.hits[0]._type", is(TYPE1)).body("hits.hits[1]._type", is(TYPE1)).when()
+				.get(new URL(context, SEARCH_REST_API).toExternalForm()).andReturn().getBody().jsonPath().getString("uuid");
 	}
+
+	// TODO #142 - add functional tests for content type level security
 
 	@Test
 	@InSequence(42)
@@ -119,34 +109,25 @@ public class SearchRestServiceTest {
 		DeploymentHelpers.refreshES();
 	}
 
-
 	@Test
 	@InSequence(50)
 	public void assertPutSearchInvalid() throws MalformedURLException {
-		given().contentType(ContentType.JSON)
-				.pathParam("search_result_uuid", "invalid-uuid")
-				.pathParam("hit_id", "invalid-hit-id")
-				.expect()
-				.statusCode(200)
-				.log().ifError()
-				.body(is("statistics record ignored"))
-				.when().put(new URL(context, SEARCH_REST_API + "/{search_result_uuid}/{hit_id}").toExternalForm());
+		given().contentType(ContentType.JSON).pathParam("search_result_uuid", "invalid-uuid")
+				.pathParam("hit_id", "invalid-hit-id").expect().statusCode(200).log().ifError()
+				.body(is("statistics record ignored")).when()
+				.put(new URL(context, SEARCH_REST_API + "/{search_result_uuid}/{hit_id}").toExternalForm());
 
 	}
 
 	@Test
-	@InSequence(51) @Ignore("REST Search API behaves differently")
+	@InSequence(51)
+	@Ignore("REST Search API behaves differently")
 	public void assertPutSearch() throws MalformedURLException {
 		// TODO: FTEST: SearchRestAPI: Investigate Put Search
-		given().contentType(ContentType.JSON)
-				.pathParam("search_result_uuid", uuid)
-				.pathParam("hit_id", TYPE1 + "-" + contentId)
-				.log().all()
-				.expect()
-				.log().all()
-				.statusCode(200)
-				.body(is("statistics record accepted"))
-				.when().put(new URL(context, SEARCH_REST_API + "/{search_result_uuid}/{hit_id}").toExternalForm());
+		given().contentType(ContentType.JSON).pathParam("search_result_uuid", uuid)
+				.pathParam("hit_id", TYPE1 + "-" + contentId).log().all().expect().log().all().statusCode(200)
+				.body(is("statistics record accepted")).when()
+				.put(new URL(context, SEARCH_REST_API + "/{search_result_uuid}/{hit_id}").toExternalForm());
 	}
 
 }
