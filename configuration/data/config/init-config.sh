edebbac9e683adc1f5ed4f3d6efc2929af187665
Libor Krzyzanek <libor@krzyzanek.com>
2014-10-17 13:18:56 +0200

    Overrides URL for pushing configs on Openshift

--- configuration/data/config/init-config.sh
+++ configuration/data/config/init-config.sh
@@ -14,6 +14,9 @@
 clear
 
 sysurl=https://dcp-jbossorgdev.rhcloud.com
+if [ -n "${OPENSHIFT_JBOSSEAP_IP}" ]; then
+  sysurl=http://${OPENSHIFT_JBOSSEAP_IP}:8080
+fi
 if [ -n "$1" ]; then
   sysurl=$1
 fi
